1fac6eac8760b4d45914d25483ae2e503a84ca7a
Olga Berezhna <57196165+oberezhnay@users.noreply.github.com>
2025-10-03 12:04:28 +0300

    Solution

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,21 @@
 import { createRoot } from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { HashRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { HomePage } from './pages/HomePage';
+import { TabsPage } from './pages/TabsPage';
 import { App } from './App';
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <HashRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<HomePage />} />
+        <Route path="tabs">
+          <Route index element={<TabsPage />} />
+          <Route path=":tabId" element={<TabsPage />} />
+        </Route>
+        <Route path="home" element={<Navigate to="/" replace />} />
+        <Route path="*" element={<h1 className="title">Page not found</h1>} />
+      </Route>
+    </Routes>
   </HashRouter>,
 );

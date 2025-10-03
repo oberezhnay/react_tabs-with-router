82a962fd9238e293ac1edaafbd1d97c642bf5bf4
Olga Berezhna <57196165+oberezhnay@users.noreply.github.com>
2025-10-03 13:44:47 +0300

    Add fixes to home and Tabs link classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,44 @@
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
-import { Link, Outlet, useLocation } from 'react-router-dom';
+import { NavLink, Outlet, useLocation } from 'react-router-dom';
 import classNames from 'classnames';
 
 export const App = () => {
   const location = useLocation();
 
+  const getLinkClassHome = () =>
+    classNames({
+      'is-active': location.pathname === '/',
+    });
+
+  const getLinkClassTabs = () =>
+    classNames({
+      'is-active': location.pathname.startsWith('/tabs'),
+    });
+
   return (
     <>
-      {/* Also requires <html class="has-navbar-fixed-top"> */}
       <nav
         className="navbar is-light is-fixed-top is-mobile has-shadow"
         data-cy="Nav"
       >
         <div className="container">
           <div className="navbar-brand">
-            <Link to="/" className={classNames('navbar-item', {'is-active': location.pathname === '/',})}>
-              Home
-            </Link>
-            <Link to="/tabs" className={classNames('navbar-item', {'is-active': location.pathname.startsWith('/tabs'),})}>
-              Tabs
-            </Link>
+            <div
+              className={getLinkClassHome()}
+            >
+              <NavLink to="/" className='navbar-item'>
+                Home
+              </NavLink>
+            </div>
+            <div
+              className={getLinkClassTabs()}
+            >
+              <NavLink to="/tabs" className='navbar-item'>
+                Tabs
+              </NavLink>
+            </div>
           </div>
         </div>
       </nav>
